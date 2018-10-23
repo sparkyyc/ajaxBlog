2748777ef5d1676069bb7bdc84ee530b640de9e9
Christa Sparks <christa@Christas-MacBook-Pro.local>
2018-10-22 21:12:14 -0600

    tweaks

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+var createError = require('http-errors')
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -9,11 +11,12 @@ var postRouter = require('./routes/post')
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -33,4 +36,4 @@ app.use(function(req, res, next) {
     res.render('error');
   });
 
-module.exports = app;
+module.exports = app
--- routes/post.js
+++ routes/post.js
@@ -3,22 +3,81 @@ const router = express.Router()
 const knex = require('../knex')
 // READ ALL records for this table
 router.get('/', (req, res, next) => {
-  res.send('ALL RECORDS')
+    knex('post')
+        .then((rows) => {
+            res.json(rows)
+        })
+        .catch((err) => {
+            next(err)
+        })
 })
 // READ ONE record for this table
 router.get('/:id', (req, res, next) => {
-  res.send('ONE RECORD')
+    knex('post')
+        .where('id', req.params.id)
+        .then((rows) => {
+            res.json(rows)
+        })
+        .catch((err) => {
+            next(err)
+        })
 })
 // CREATE ONE record for this table
 router.post('/', (req, res, next) => {
-  res.send('CREATED RECORD')
+    knex('post')
+        .insert({
+            "title": req.body.title,
+            "content": req.body.content
+        })
+        .returning('*')
+        .then((data) => {
+            res.json(data[0])
+        })
+        .catch((err) => {
+            next(err)
+        })
 })
 // UPDATE ONE record for this table
 router.put('/:id', (req, res, next) => {
-  res.send('UPDATED RECORD')
+    knex('post')
+        .where('id', req.params.id)
+        .then((data) => {
+            if (data.length > 0) {
+                let record = data[0]
+                if (req.body.title) record.title = req.body.title
+                if (req.body.content) record.content = req.body.content
+
+                knex('post')
+                    .update(record)
+                    .where('id', req.params.id)
+                    .returning('*')
+                    .then((update) => {
+                        res.json(update)
+                    })
+            } else {
+                throw new Error ('Post not found')
+            }
+        })
+        .catch((err) => {
+            next(err)
+        })
 })
 // DELETE ONE record for this table
-router.delete('/:id', (req, res, next) => {
-  res.send('DELETED RECORD')
-})
+router.delete('/:id', function(req, res, next) {
+    knex('post')
+      .where('id', req.params.id)
+      .first()
+      .then((row) => {
+        if(!row) return next()
+        knex('post')
+          .del()
+          .where('id', req.params.id)
+          .then(() => {
+            res.send(`ID ${req.params.id} Deleted`)
+          })
+      })
+      .catch((err) => {
+        next(err)
+      })
+  })
 module.exports = router
